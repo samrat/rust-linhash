ce67badff0963796467e091dab9c56596e19f21b
Samrat Man Singh <samratmansingh@gmail.com>
2017-11-09 11:06:42 +0545

    Fix tests

--- src/disk.rs
+++ src/disk.rs
@@ -438,6 +438,7 @@ impl DbFile {
 
 #[cfg(test)]
 mod tests {
+    use disk;
     use DbFile;
     use std::fs;
 
@@ -448,15 +449,15 @@ mod tests {
         let krab = b"krab";
         // write to page 1
         bp.write_record(1, 14, bark, krab);
-        assert_eq!(bp.buffers[NUM_BUFFERS-1].read_record(14),
-                   (bark, krab));
+        assert_eq!(bp.buffers[disk::NUM_BUFFERS-1].read_record(14),
+                   (&bark[..], &krab[..]));
         bp.close();
 
         let mut bp2 = DbFile::new("/tmp/dbfile_tests", 4, 4);
         // read from page 1
         let buffer_index = bp2.fetch_page(1);
         assert_eq!(bp2.buffers[buffer_index].read_record(14),
-                   (bark, krab));
+                   (&bark[..], &krab[..]));
 
         fs::remove_file("/tmp/dbfile_tests").ok();
     }
